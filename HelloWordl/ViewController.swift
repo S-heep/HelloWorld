815af27c3a02158cdfa134e014e8035b45e80cb3
S-heep <991207691@qq.com>
2018-01-16 10:09:04 +0800

    添加自控件

--- HelloWordl/ViewController.swift
+++ HelloWordl/ViewController.swift
@@ -12,12 +12,18 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = UIColor.gray
+        let button = UIButton.init(type: .custom)
+        button.frame = CGRect(x: 100, y: 200, width: 200, height: 44)
+        button.setTitle("HelloWorld", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        view.addSubview(button)
+        button.backgroundColor = .white
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
     }
 
 
